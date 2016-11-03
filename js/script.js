996ef097759aabe09d7717b447ec8fc7afaccad4
Dana Yazzie <redcloudsaboveus01@gmail.com>
2016-11-03 11:19:24 -0600

    added an h2 tag to index and i also added more stuff in my script page

--- js/script.js
+++ js/script.js
@@ -3,6 +3,28 @@ var url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=
 $.ajax({
   url: url,
   success: function(results) {
-    alert("Got back " + results.element_count + " asteroids");
+    $("#count").html(results.element_count);
+    // Below does the exact same thing, but more longer / ungainly
+    // document.getElementById("count").innerHTML = results.element_count;
+
+    var asteroids = results.near_earth_objects["2015-09-08"];
+    var asteroid;
+    var currentRow;
+    for(var i = 0; i < asteroids.length; i++) {
+
+      if(i % 3 === 0) {
+        currentRow = $("<div></div>").addClass("row");
+        $(".container").append(currentRow);
+      }
+      makeName(asteroids[i], currentRow);
+    }
   }
 });
+
+function makeName(asteroid){
+  var column = $("<div></div>").addClass("col-md-4");
+  var name = $("<h3></h3>").html(asteroid.name);
+
+  $(column).append(name);
+  $(row).append(column);
+}
