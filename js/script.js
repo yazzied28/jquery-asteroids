511a377c5018fbfcb778b615646817da3cb46151
Dana Yazzie <redcloudsaboveus01@gmail.com>
2016-11-03 09:33:14 -0600

    acquired an API key from NASA website and put it in my script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY";
+var url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=XqFf3eilW1Et6L9C4TQ5dQ86981yiMCpa85Is9i3";
 
 $.ajax({
   url: url,
