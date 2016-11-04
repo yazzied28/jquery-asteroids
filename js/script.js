124fca5e5e6b53941fd5792abac73c7baf349ab0
Dana Yazzie <redcloudsaboveus01@gmail.com>
2016-11-04 11:52:35 -0600

    added some more script in the js, added a style.css file, added rows and columns

--- js/script.js
+++ js/script.js
@@ -12,19 +12,51 @@ $.ajax({
     var currentRow;
     for(var i = 0; i < asteroids.length; i++) {
 
-      if(i % 3 === 0) {
-        currentRow = $("<div></div>").addClass("row");
-        $(".container").append(currentRow);
-      }
+      currentRow = $("<div></div>").addClass("row");
+      $(".container").append(currentRow);
+
       makeName(asteroids[i], currentRow);
+      makeSize(asteroids[i], currentRow);
+      makeClose(asteroids[i], currentRow);
     }
   }
 });
 
-function makeName(asteroid){
+function makeName(asteroid, row){
   var column = $("<div></div>").addClass("col-md-4");
   var name = $("<h3></h3>").html(asteroid.name);
 
+
   $(column).append(name);
+  $(row).append(column);
+}
+
+function makeSize(asteroid, row) {
+  var column = $("<div></div>").addClass("col-md-4");
+  // TODO I should calculate the average size of the asteroid, rather than just
+  // use it's max diameter
+  var size = $("<h4></h4>").html(asteroid.estimated_diameter.feet.estimated_diameter_max);
+
+  $(column).append(size);
+  $(row).append(column);
+}
+
+// This function is used to create the third couumn, which contains close
+// approach data about this asteroid.
+function makeClose(asteroid, row) {
+  var column = $("<div></div>").addClass("col-md-4");
+  var speed = $("<h4></h4>").html(asteroid.close_approach_data[0].relative_velocity.miles_per_hour);
+  var distance = $("<h4></h4>").html(asteroid.close_approach_data[0].miss_distance.miles);
+
+  var hazard = $("<h4></h4>");
+    if(asteroid.is_potentially_hazardous_asteroid) {
+      $(hazard).html("Dangerous").addClass("text-danger");
+    } else {
+      $(hazard).html("Safe").addClass("text-success");
+    }
+
+  $(column).append(speed);
+  $(column).append(distance);
+  $(column).append(hazard);
   $(row).append(column);
 }
